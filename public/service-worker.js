46365ff922912ae7df73a9f700efa380ddacaa85
caseylister <caseynlister@gmail.com>
2022-01-10 16:07:33 -0500

    intercept fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,7 +21,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
+            console.log('Installing cache: ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
     )
@@ -37,10 +37,26 @@ self.addEventListener('activate', function (e) {
 
             return Promise.all(keyList.map(function (key, i) {
                 if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
+                    console.log('Deleting cache: ' + keyList[i] );
                     return caches.delete(keyList[i]);
                 }
             }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
         })
     )
 });

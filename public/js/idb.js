fb40b945c8206b558845c798d0c4eb72aa7ec3b3
caseylister <caseynlister@gmail.com>
2022-01-10 16:21:30 -0500

    idb connection

--- public/js/idb.js
+++ public/js/idb.js
@@ -12,4 +12,21 @@ request.onupgradeneeded = function(event) {
     db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
 
+  
